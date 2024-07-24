f56546bc8c6c3854ac69d9dee2af2e09aade0451
= <=>
2024-07-24 14:51:14 +0000

    refactor

--- src/components/audioEngine/master.logic.ts
+++ src/components/audioEngine/master.logic.ts
@@ -24,42 +24,28 @@ export const masterAudioOutput: MasterAudioTypes = {
     volume: applyDbReduction(100, dbBoost)
 };
 
-export function createGainNode(elem: HTMLMediaElement) {
-    if (!masterAudioOutput.audioContext || !masterAudioOutput.mixerNode) {
-        console.error('MasterAudio is not initialized');
-        return;
-    }
-
-    const source = masterAudioOutput.audioContext.createMediaElementSource(elem);
-
-    audioNodeBus.unshift(masterAudioOutput.audioContext.createGain());
-    audioNodeBus[0].gain.setValueAtTime(0, masterAudioOutput.audioContext.currentTime);
-
-    source.connect(audioNodeBus[0]);
-    audioNodeBus[0].connect(masterAudioOutput.mixerNode);
-}
-
 export let unbindCallback = () => {
     return;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function initializeMasterAudio(unbind: any) {
+    const savedDuration = userSettings.crossfadeDuration(undefined);
+    setXDuration(savedDuration);
+
     unbindCallback = unbind;
 
     const webAudioSupported = ('AudioContext' in window || 'webkitAudioContext' in window);
 
     if (!webAudioSupported) {
-        throw new Error('WebAudio not supported');
+        console.log('WebAudio not supported');
+        return;
     }
     // eslint-disable-next-line compat/compat, @typescript-eslint/no-explicit-any
     const AudioContext = window.AudioContext || (window as any).webkitAudioContext;
     const audioCtx = masterAudioOutput.audioContext || new AudioContext();
 
     if (!masterAudioOutput.audioContext) masterAudioOutput.audioContext = audioCtx;
-
-    const savedDuration = userSettings.crossfadeDuration(undefined);
-    setXDuration(savedDuration);
 
     if (!masterAudioOutput.mixerNode) {
         masterAudioOutput.mixerNode = audioCtx.createGain();
@@ -73,3 +59,20 @@ export function initializeMasterAudio(unbind: any) {
 type GainNodes = GainNode[];
 
 export const audioNodeBus: GainNodes = [];
+
+export function createGainNode(elem: HTMLMediaElement) {
+    if (!masterAudioOutput.audioContext || !masterAudioOutput.mixerNode) {
+        console.log('MasterAudio is not initialized');
+        return;
+    }
+
+    const source = masterAudioOutput.audioContext.createMediaElementSource(elem);
+    const gainNode = masterAudioOutput.audioContext.createGain();
+
+    audioNodeBus.unshift(gainNode);
+    audioNodeBus[0].gain.setValueAtTime(0, masterAudioOutput.audioContext.currentTime);
+
+    source.connect(audioNodeBus[0]);
+    audioNodeBus[0].connect(masterAudioOutput.mixerNode);
+}
+
