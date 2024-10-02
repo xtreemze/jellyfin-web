cffa3b052dc0c0198095c7e5271e3f2a93c135da
= <=>
2024-10-02 11:45:33 +0000

    adjust font size for analyzer

--- src/components/visualizer/FrequencyAnalyzer.tsx
+++ src/components/visualizer/FrequencyAnalyzer.tsx
@@ -176,9 +176,9 @@ const FrequencyAnalyzer: React.FC<FrequencyAnalyzersProps> = ({
                 }
 
                 // Adjust font size based on canvas height
-                const fontSize = Math.max(10, canvasHeight / 50);
-                ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
-                ctx.font = `${fontSize}px Arial`;
+                const fontSize = Math.max(10, canvasHeight / 75);
+                ctx.fillStyle = 'rgba(255, 255, 255, 0.6)';
+                ctx.font = `${fontSize}px Roboto`;
                 ctx.textAlign = 'center';
                 ctx.textBaseline = 'middle';
 
