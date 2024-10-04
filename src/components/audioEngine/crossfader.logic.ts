1dc9d11f7f685107afb8f12d99317753d3069dc5
= <=>
2024-10-04 18:02:54 +0000

    reduce crossfade sustain

--- src/components/audioEngine/crossfader.logic.ts
+++ src/components/audioEngine/crossfader.logic.ts
@@ -23,7 +23,7 @@ export function setXDuration(crossfadeDuration: number) {
     }
 
     xDuration.enabled = true;
-    xDuration.fadeOut = crossfadeDuration * 2.5;
+    xDuration.fadeOut = crossfadeDuration * 2;
     xDuration.disableFade = false;
     xDuration.sustain = crossfadeDuration;
 }
