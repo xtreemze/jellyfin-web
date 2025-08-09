8c36d3b9297dd1d3956d808f36c8ca29c2ca2200
Carlos Velasco <8251335+xtreemze@users.noreply.github.com>
2025-08-08 21:39:54 -0600

    Update crossfader.logic.ts
    fade out to 2x from 3x

--- src/components/audioEngine/crossfader.logic.ts
+++ src/components/audioEngine/crossfader.logic.ts
@@ -27,7 +27,7 @@ export function setXDuration(crossfadeDuration: number) {
     }
 
     xDuration.enabled = true;
-    xDuration.fadeOut = crossfadeDuration * 3;
+    xDuration.fadeOut = crossfadeDuration * 2;
     xDuration.disableFade = false;
     xDuration.sustain = crossfadeDuration;
 }
